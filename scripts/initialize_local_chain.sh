951759cffbed1b1247c1c31d83cb46012b857627
Byron Kim Barra <byronbarra09@gmail.com>
2024-10-17 23:46:05 +0800

    couple of renames and organized docker folder volumes to mimic repo

--- scripts/initialize_local_chain.sh
+++ scripts/initialize_local_chain.sh
@@ -27,55 +27,55 @@ keyname=admin
 #  -p 14269:14269 \
 #  -p 9411:9411 \
 #  jaegertracing/all-in-one:1.33
-# clean up old sei directory
-rm -rf ~/.sei
+# clean up old kiichain directory
+rm -rf ~/.kiichain3
 echo "Building..."
-#install seid
+#install kiichaind
 make install
 # initialize chain with chain ID and add the first key
-~/go/bin/seid init demo --chain-id sei-chain
-~/go/bin/seid keys add $keyname --keyring-backend test
+~/go/bin/kiichaind init demo --chain-id kiichain3
+~/go/bin/kiichaind keys add $keyname --keyring-backend test
 # add the key as a genesis account with massive balances of several different tokens
-~/go/bin/seid add-genesis-account $(~/go/bin/seid keys show $keyname -a --keyring-backend test) 100000000000000000000usei,100000000000000000000uusdc,100000000000000000000uatom --keyring-backend test
+~/go/bin/kiichaind add-genesis-account $(~/go/bin/kiichaind keys show $keyname -a --keyring-backend test) 100000000000000000000ukii,100000000000000000000uusdc,100000000000000000000uatom --keyring-backend test
 # gentx for account
-~/go/bin/seid gentx $keyname 7000000000000000usei --chain-id sei-chain --keyring-backend test
+~/go/bin/kiichaind gentx $keyname 7000000000000000ukii --chain-id kiichain3 --keyring-backend test
 # add validator information to genesis file
-KEY=$(jq '.pub_key' ~/.sei/config/priv_validator_key.json -c)
-jq '.validators = [{}]' ~/.sei/config/genesis.json > ~/.sei/config/tmp_genesis.json
-jq '.validators[0] += {"power":"7000000000"}' ~/.sei/config/tmp_genesis.json > ~/.sei/config/tmp_genesis_2.json
-jq '.validators[0] += {"pub_key":'$KEY'}' ~/.sei/config/tmp_genesis_2.json > ~/.sei/config/tmp_genesis_3.json
-mv ~/.sei/config/tmp_genesis_3.json ~/.sei/config/genesis.json && rm ~/.sei/config/tmp_genesis.json && rm ~/.sei/config/tmp_genesis_2.json
+KEY=$(jq '.pub_key' ~/.kiichain3/config/priv_validator_key.json -c)
+jq '.validators = [{}]' ~/.kiichain3/config/genesis.json > ~/.kiichain3/config/tmp_genesis.json
+jq '.validators[0] += {"power":"7000000000"}' ~/.kiichain3/config/tmp_genesis.json > ~/.kiichain3/config/tmp_genesis_2.json
+jq '.validators[0] += {"pub_key":'$KEY'}' ~/.kiichain3/config/tmp_genesis_2.json > ~/.kiichain3/config/tmp_genesis_3.json
+mv ~/.kiichain3/config/tmp_genesis_3.json ~/.kiichain3/config/genesis.json && rm ~/.kiichain3/config/tmp_genesis.json && rm ~/.kiichain3/config/tmp_genesis_2.json
 
 echo "Creating Accounts"
 # create 10 test accounts + fund them
 python3  loadtest/scripts/populate_genesis_accounts.py 20 loc
 
-~/go/bin/seid collect-gentxs
+~/go/bin/kiichaind collect-gentxs
 # update some params in genesis file for easier use of the chain localls (make gov props faster)
-cat ~/.sei/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["max_deposit_period"]="60s"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
-cat ~/.sei/config/genesis.json | jq '.app_state["gov"]["voting_params"]["voting_period"]="30s"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
-cat ~/.sei/config/genesis.json | jq '.app_state["gov"]["voting_params"]["expedited_voting_period"]="10s"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
-cat ~/.sei/config/genesis.json | jq '.app_state["oracle"]["params"]["vote_period"]="2"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
-cat ~/.sei/config/genesis.json | jq '.app_state["oracle"]["params"]["whitelist"]=[{"name": "ueth"},{"name": "ubtc"},{"name": "uusdc"},{"name": "uusdt"},{"name": "uosmo"},{"name": "uatom"},{"name": "usei"}]' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
-cat ~/.sei/config/genesis.json | jq '.app_state["distribution"]["params"]["community_tax"]="0.000000000000000000"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
-cat ~/.sei/config/genesis.json | jq '.consensus_params["block"]["max_gas"]="35000000"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
-cat ~/.sei/config/genesis.json | jq '.app_state["staking"]["params"]["max_voting_power_ratio"]="1.000000000000000000"' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
-cat ~/.sei/config/genesis.json | jq '.app_state["bank"]["denom_metadata"]=[{"denom_units":[{"denom":"usei","exponent":0,"aliases":["USEI"]}],"base":"usei","display":"usei","name":"USEI","symbol":"USEI"}]' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
+cat ~/.kiichain3/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["max_deposit_period"]="60s"' > ~/.kiichain3/config/tmp_genesis.json && mv ~/.kiichain3/config/tmp_genesis.json ~/.kiichain3/config/genesis.json
+cat ~/.kiichain3/config/genesis.json | jq '.app_state["gov"]["voting_params"]["voting_period"]="30s"' > ~/.kiichain3/config/tmp_genesis.json && mv ~/.kiichain3/config/tmp_genesis.json ~/.kiichain3/config/genesis.json
+cat ~/.kiichain3/config/genesis.json | jq '.app_state["gov"]["voting_params"]["expedited_voting_period"]="10s"' > ~/.kiichain3/config/tmp_genesis.json && mv ~/.kiichain3/config/tmp_genesis.json ~/.kiichain3/config/genesis.json
+cat ~/.kiichain3/config/genesis.json | jq '.app_state["oracle"]["params"]["vote_period"]="2"' > ~/.kiichain3/config/tmp_genesis.json && mv ~/.kiichain3/config/tmp_genesis.json ~/.kiichain3/config/genesis.json
+cat ~/.kiichain3/config/genesis.json | jq '.app_state["oracle"]["params"]["whitelist"]=[{"name": "ueth"},{"name": "ubtc"},{"name": "uusdc"},{"name": "uusdt"},{"name": "uosmo"},{"name": "uatom"},{"name": "ukii"}]' > ~/.kiichain3/config/tmp_genesis.json && mv ~/.kiichain3/config/tmp_genesis.json ~/.kiichain3/config/genesis.json
+cat ~/.kiichain3/config/genesis.json | jq '.app_state["distribution"]["params"]["community_tax"]="0.000000000000000000"' > ~/.kiichain3/config/tmp_genesis.json && mv ~/.kiichain3/config/tmp_genesis.json ~/.kiichain3/config/genesis.json
+cat ~/.kiichain3/config/genesis.json | jq '.consensus_params["block"]["max_gas"]="35000000"' > ~/.kiichain3/config/tmp_genesis.json && mv ~/.kiichain3/config/tmp_genesis.json ~/.kiichain3/config/genesis.json
+cat ~/.kiichain3/config/genesis.json | jq '.app_state["staking"]["params"]["max_voting_power_ratio"]="1.000000000000000000"' > ~/.kiichain3/config/tmp_genesis.json && mv ~/.kiichain3/config/tmp_genesis.json ~/.kiichain3/config/genesis.json
+cat ~/.kiichain3/config/genesis.json | jq '.app_state["bank"]["denom_metadata"]=[{"denom_units":[{"denom":"ukii","exponent":0,"aliases":["USEI"]}],"base":"ukii","display":"ukii","name":"USEI","symbol":"USEI"}]' > ~/.kiichain3/config/tmp_genesis.json && mv ~/.kiichain3/config/tmp_genesis.json ~/.kiichain3/config/genesis.json
 
 # Use the Python command to get the dates
 START_DATE=$($PYTHON_CMD -c "from datetime import datetime; print(datetime.now().strftime('%Y-%m-%d'))")
 END_DATE_3DAYS=$($PYTHON_CMD -c "from datetime import datetime, timedelta; print((datetime.now() + timedelta(days=3)).strftime('%Y-%m-%d'))")
 END_DATE_5DAYS=$($PYTHON_CMD -c "from datetime import datetime, timedelta; print((datetime.now() + timedelta(days=5)).strftime('%Y-%m-%d'))")
 
-cat ~/.sei/config/genesis.json | jq --arg start_date "$START_DATE" --arg end_date "$END_DATE_3DAYS" '.app_state["mint"]["params"]["token_release_schedule"]=[{"start_date": $start_date, "end_date": $end_date, "token_release_amount": "999999999999"}]' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
-cat ~/.sei/config/genesis.json | jq --arg start_date "$END_DATE_3DAYS" --arg end_date "$END_DATE_5DAYS" '.app_state["mint"]["params"]["token_release_schedule"] += [{"start_date": $start_date, "end_date": $end_date, "token_release_amount": "999999999999"}]' > ~/.sei/config/tmp_genesis.json && mv ~/.sei/config/tmp_genesis.json ~/.sei/config/genesis.json
+cat ~/.kiichain3/config/genesis.json | jq --arg start_date "$START_DATE" --arg end_date "$END_DATE_3DAYS" '.app_state["mint"]["params"]["token_release_schedule"]=[{"start_date": $start_date, "end_date": $end_date, "token_release_amount": "999999999999"}]' > ~/.kiichain3/config/tmp_genesis.json && mv ~/.kiichain3/config/tmp_genesis.json ~/.kiichain3/config/genesis.json
+cat ~/.kiichain3/config/genesis.json | jq --arg start_date "$END_DATE_3DAYS" --arg end_date "$END_DATE_5DAYS" '.app_state["mint"]["params"]["token_release_schedule"] += [{"start_date": $start_date, "end_date": $end_date, "token_release_amount": "999999999999"}]' > ~/.kiichain3/config/tmp_genesis.json && mv ~/.kiichain3/config/tmp_genesis.json ~/.kiichain3/config/genesis.json
 
 if [ ! -z "$2" ]; then
   APP_TOML_PATH="$2"
 else
-  APP_TOML_PATH="$HOME/.sei/config/app.toml"
+  APP_TOML_PATH="$HOME/.kiichain3/config/app.toml"
 fi
-# Enable OCC and SeiDB
+# Enable OCC and KiichainDB
 sed -i.bak -e 's/# concurrency-workers = .*/concurrency-workers = 500/' $APP_TOML_PATH
 sed -i.bak -e 's/occ-enabled = .*/occ-enabled = true/' $APP_TOML_PATH
 sed -i.bak -e 's/sc-enable = .*/sc-enable = true/' $APP_TOML_PATH
@@ -86,13 +86,13 @@ sed -i.bak -e 's/ss-enable = .*/ss-enable = true/' $APP_TOML_PATH
 if [ ! -z "$1" ]; then
   CONFIG_PATH="$1"
 else
-  CONFIG_PATH="$HOME/.sei/config/config.toml"
+  CONFIG_PATH="$HOME/.kiichain3/config/config.toml"
 fi
 
 if [ ! -z "$2" ]; then
   APP_PATH="$2"
 else
-  APP_PATH="$HOME/.sei/config/app.toml"
+  APP_PATH="$HOME/.kiichain3/config/app.toml"
 fi
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
@@ -122,7 +122,7 @@ else
   exit 1
 fi
 
-~/go/bin/seid config keyring-backend test
+~/go/bin/kiichaind config keyring-backend test
 
 if [ $NO_RUN = 1 ]; then
   echo "No run flag set, exiting without starting the chain"
@@ -130,4 +130,4 @@ if [ $NO_RUN = 1 ]; then
 fi
 
 # start the chain with log tracing
-GORACE="log_path=/tmp/race/seid_race" ~/go/bin/seid start --trace --chain-id sei-chain
+GORACE="log_path=/tmp/race/kiichaind_race" ~/go/bin/kiichaind start --trace --chain-id kiichain3
