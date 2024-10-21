9582fec168cc3b637e7ca4d783b30b4b40833c30
matteyu <matt.r.ilagan@gmail.com>
2024-10-20 20:14:29 -0700

    remove mainnet terraform dir

--- terraform/mainnet/validator/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "The AWS region to launch instances."
-  default     = "us-east-2"
-}
-
-variable "instance_count" {
-  description = "Number of validator instances to create."
-  default     = 1
-}
