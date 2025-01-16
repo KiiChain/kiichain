29e721855bcf78ca4c35ac085aa0ed1134ab25b9
Jhelison Uchoa <jhelisong@gmail.com>
2025-01-16 14:57:13 -0300

    chore: Fix dApp funding account

--- integration_test/contracts/fund_daap_account.sh
+++ integration_test/contracts/fund_daap_account.sh
@@ -1,15 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
 
-kiichaindbin=$(which ~/go/bin/kiichaind | tr -d '"')
-keyname=$(printf "12345678\n" | $kiichaindbin keys list --output json | jq ".[0].name" | tr -d '"')
-keyaddress=$(printf "12345678\n" | $kiichaindbin keys list --output json | jq ".[0].address" | tr -d '"')
-chainid=$($kiichaindbin status | jq ".NodeInfo.network" | tr -d '"')
-kiihome=$(git rev-parse --show-toplevel | tr -d '"')
-DAPP_ACCOUNT="kii1zlvjwlwl967gpdha3a3cn5u0hvfurnnuu52wvj"
-
-cd $kiihome || exit
 echo "Funding dApp account..."
+ADMIN="admin"
+DAPP_ADDRESS="kii1zlvjwlwl967gpdha3a3cn5u0hvfurnnuu52wvj"
 
-$kiichaindbin tx bank send $keyname $DAPP_ACCOUNT 100000000000ukii -b block --fees 2000ukii --chain-id kii -y --output json
+printf "12345678\n" | kiichaind tx bank send $ADMIN $DAPP_ADDRESS 100000000000ukii -b block --fees 2000ukii --chain-id kii -y --output json
 
 echo "dApp account funded with 100000000000ukii"
