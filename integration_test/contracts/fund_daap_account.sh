b28b189d179a4ede07e5cf0483ee67e9dbfbd53c
Jhelison Uchoa <jhelisong@gmail.com>
2025-01-16 14:18:49 -0300

    chore: Add fund dapp account and mnemonic

---
+++ integration_test/contracts/fund_daap_account.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+kiichaindbin=$(which ~/go/bin/kiichaind | tr -d '"')
+keyname=$(printf "12345678\n" | $kiichaindbin keys list --output json | jq ".[0].name" | tr -d '"')
+keyaddress=$(printf "12345678\n" | $kiichaindbin keys list --output json | jq ".[0].address" | tr -d '"')
+chainid=$($kiichaindbin status | jq ".NodeInfo.network" | tr -d '"')
+kiihome=$(git rev-parse --show-toplevel | tr -d '"')
+DAPP_ACCOUNT="kii1zlvjwlwl967gpdha3a3cn5u0hvfurnnuu52wvj"
+
+cd $kiihome || exit
+echo "Funding dApp account..."
+
+$kiichaindbin tx bank send $keyname $DAPP_ACCOUNT 100000000000ukii -b block --fees 2000ukii --chain-id kii -y --output json
