cc9bb9526750c7a4247538aa4e2e8d76ae5ccb7d
Jhelison Uchoa <jhelisong@gmail.com>
2025-01-21 11:28:54 -0300

    feat: Add 1.3.0 upgrade to the list

--- app/upgrades.go
+++ app/upgrades.go
@@ -14,7 +14,9 @@ import (
 // NOTE: When performing upgrades, make sure to keep / register the handlers
 // for both the current (n) and the previous (n-1) upgrade name. There is a bug
 // in a missing value in a log statement for which the fix is not released
-var upgradesList = []string{}
+var upgradesList = []string{
+	"1.3.0",
+}
 
 // if there is an override list, use that instead, for integration tests
 func overrideList() {
