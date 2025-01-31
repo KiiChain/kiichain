f6ca14175dcbea1ada5bab55cefc34f00ee36ff0
Jhelison Uchoa <jhelisong@gmail.com>
2025-01-31 18:37:04 -0300

    feat: update keeper with new logic for error handling

--- x/mint/keeper/hooks.go
+++ x/mint/keeper/hooks.go
@@ -5,37 +5,56 @@ import (
 	epochTypes "github.com/kiichain/kiichain3/x/epoch/types"
 )
 
+// BeforeEpochStart is a hook that's ran after an epoch starts
 func (k Keeper) BeforeEpochStart(_ sdk.Context, _ epochTypes.Epoch) {}
 
+// AfterEpochEnd is a hook that's ran after an epoch ends
 func (k Keeper) AfterEpochEnd(ctx sdk.Context, epoch epochTypes.Epoch) {
-	latestMinter := k.GetOrUpdateLatestMinter(ctx, epoch)
-	coinsToMint := latestMinter.GetReleaseAmountToday(epoch.CurrentEpochStartTime.UTC())
+	// Get the latest minter
+	latestMinter, err := k.GetOrUpdateLatestMinter(ctx, epoch)
+	if err != nil {
+		// We can panic, it's common for hooks to panic
+		// The panic is captured on epoch hooks
+		panic(err)
+	}
 
+	// Get the coinsToMint
+	coinsToMint, err := latestMinter.GetReleaseAmountToday(epoch.CurrentEpochStartTime.UTC())
+	if err != nil {
+		panic(err)
+	}
+
+	// Get the remaining mint amount
 	if coinsToMint.IsZero() || latestMinter.GetRemainingMintAmount() == 0 {
 		k.Logger(ctx).Debug("No coins to mint", "minter", latestMinter)
-		return
 	}
 
 	// mint coins, update supply
 	if err := k.MintCoins(ctx, coinsToMint); err != nil {
+		// We can panic, it's common for hooks to panic
+		// The panic is captured, logged and handled on epoch hooks
 		panic(err)
 	}
 	// send the minted coins to the fee collector account
 	if err := k.AddCollectedFees(ctx, coinsToMint); err != nil {
+		// We can panic, it's common for hooks to panic
+		// The panic is captured, logged and handled on epoch hooks
 		panic(err)
 	}
 
-	// Released Succssfully, decrement the remaining amount by the daily release amount and update minter
+	// Released successfully, decrement the remaining amount by the daily release amount and update minter
 	amountMinted := coinsToMint.AmountOf(latestMinter.GetDenom())
 	latestMinter.RecordSuccessfulMint(ctx, epoch, amountMinted.Uint64())
 	k.Logger(ctx).Info("Minted coins", "minter", latestMinter, "amount", coinsToMint.String())
 	k.SetMinter(ctx, latestMinter)
 }
 
+// Hooks is the hook struct for the mint module
 type Hooks struct {
 	k Keeper
 }
 
+// Assert the interface
 var _ epochTypes.EpochHooks = Hooks{}
 
 // Return the wrapper struct.
@@ -43,11 +62,12 @@ func (k Keeper) Hooks() Hooks {
 	return Hooks{k}
 }
 
-// epochs hooks.
+// Epoch hook for before epoch start
 func (h Hooks) BeforeEpochStart(ctx sdk.Context, epoch epochTypes.Epoch) {
 	h.k.BeforeEpochStart(ctx, epoch)
 }
 
+// Epoch hook for before after start
 func (h Hooks) AfterEpochEnd(ctx sdk.Context, epoch epochTypes.Epoch) {
 	h.k.AfterEpochEnd(ctx, epoch)
 }
