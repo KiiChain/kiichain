0816f7826cb338143e37b78bb3e24fc59a2bfec9
Jhelison Uchoa <jhelisong@gmail.com>
2025-02-04 10:08:58 -0300

    refactor: Remove unused function getDenomsFromCreator

--- x/tokenfactory/keeper/creators.go
+++ x/tokenfactory/keeper/creators.go
@@ -9,19 +9,6 @@ func (k Keeper) addDenomFromCreator(ctx sdk.Context, creator, denom string) {
 	store.Set([]byte(denom), []byte(denom))
 }
 
-func (k Keeper) getDenomsFromCreator(ctx sdk.Context, creator string) []string {
-	store := k.GetCreatorPrefixStore(ctx, creator)
-
-	iterator := store.Iterator(nil, nil)
-	defer iterator.Close()
-
-	denoms := []string{}
-	for ; iterator.Valid(); iterator.Next() {
-		denoms = append(denoms, string(iterator.Key()))
-	}
-	return denoms
-}
-
 func (k Keeper) GetAllDenomsIterator(ctx sdk.Context) sdk.Iterator {
 	return k.GetCreatorsPrefixStore(ctx).Iterator(nil, nil)
 }
