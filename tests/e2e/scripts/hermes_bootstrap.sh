387fd94556e781ae2fc377a480ffbd11c3f3e1ec
Jhelison Uchoa <jhelisong@gmail.com>
2025-04-14 17:52:50 -0300

    test: update hermes config file with fees and addresses

--- tests/e2e/scripts/hermes_bootstrap.sh
+++ tests/e2e/scripts/hermes_bootstrap.sh
@@ -50,15 +50,16 @@ grpc_addr = 'http://$KIICHAIN_A_E2E_VAL_HOST:9090'
 event_source = { mode = 'push', url = 'ws://$KIICHAIN_A_E2E_VAL_HOST:26657/websocket' , batch_delay = '50ms' }
 rpc_timeout = '10s'
 account_prefix = 'kii'
+address_type = { derivation = 'ethermint', proto_type = { pk_type = '/cosmos.evm.crypto.v1.ethsecp256k1.PubKey' } }
 key_name = 'rly01-kiichain-a'
 store_prefix = 'ibc'
 max_gas = 6000000
-gas_price = { price = 0.005, denom = 'akii' }
+gas_price = { price = 1000000000, denom = 'akii' }
 gas_multiplier = 1.5
 clock_drift = '1m' # to accommodate docker containers
 trusting_period = '14days'
 trust_threshold = { numerator = '1', denominator = '3' }
-dynamic_gas_price = { enabled = true, multiplier = 1.3, max = 0.05 }
+dynamic_gas_price = { enabled = true, multiplier = 1.3, max = 1000000000 }
 
 
 [[chains]]
@@ -68,15 +69,16 @@ grpc_addr = 'http://$KIICHAIN_B_E2E_VAL_HOST:9090'
 event_source = { mode = 'push', url = 'ws://$KIICHAIN_B_E2E_VAL_HOST:26657/websocket' , batch_delay = '50ms' }
 rpc_timeout = '10s'
 account_prefix = 'kii'
+address_type = { derivation = 'ethermint', proto_type = { pk_type = '/cosmos.evm.crypto.v1.ethsecp256k1.PubKey' } }
 key_name = 'rly01-kiichain-b'
 store_prefix = 'ibc'
 max_gas =  6000000
-gas_price = { price = 0.005, denom = 'akii' }
+gas_price = { price = 1000000000, denom = 'akii' }
 gas_multiplier = 1.5
 clock_drift = '1m' # to accommodate docker containers
 trusting_period = '14days'
 trust_threshold = { numerator = '1', denominator = '3' }
-dynamic_gas_price = { enabled = true, multiplier = 1.3, max = 0.05 }
+dynamic_gas_price = { enabled = true, multiplier = 1.3, max = 1000000000 }
 EOF
 
 # setup Hermes relayer configuration with zero gas_price
@@ -120,6 +122,7 @@ grpc_addr = 'http://$KIICHAIN_A_E2E_VAL_HOST:9090'
 event_source = { mode = 'push', url = 'ws://$KIICHAIN_A_E2E_VAL_HOST:26657/websocket' , batch_delay = '50ms' }
 rpc_timeout = '10s'
 account_prefix = 'kii'
+address_type = { derivation = 'ethermint', proto_type = { pk_type = '/cosmos.evm.crypto.v1.ethsecp256k1.PubKey' } }
 key_name = 'rly01-kiichain-a'
 store_prefix = 'ibc'
 max_gas = 6000000
@@ -138,6 +141,7 @@ grpc_addr = 'http://$KIICHAIN_B_E2E_VAL_HOST:9090'
 event_source = { mode = 'push', url = 'ws://$KIICHAIN_B_E2E_VAL_HOST:26657/websocket' , batch_delay = '50ms' }
 rpc_timeout = '10s'
 account_prefix = 'kii'
+address_type = { derivation = 'ethermint', proto_type = { pk_type = '/cosmos.evm.crypto.v1.ethsecp256k1.PubKey' } }
 key_name = 'rly01-kiichain-b'
 store_prefix = 'ibc'
 max_gas =  6000000
@@ -149,8 +153,10 @@ trust_threshold = { numerator = '1', denominator = '3' }
 dynamic_gas_price = { enabled = true, multiplier = 1.3, max = 0.05 }
 EOF
 
-
 # import keys
-hermes keys add  --key-name rly01-kiichain-b  --chain $KIICHAIN_B_E2E_CHAIN_ID --mnemonic-file /root/.hermes/KIICHAIN_B_E2E_RLY_MNEMONIC.txt
+hermes keys add --hd-path "m/44'/60'/0'/0/0" --key-name rly01-kiichain-b  --chain $KIICHAIN_B_E2E_CHAIN_ID --mnemonic-file /root/.hermes/KIICHAIN_B_E2E_RLY_MNEMONIC.txt 
 sleep 5
-hermes keys add  --key-name rly01-kiichain-a  --chain $KIICHAIN_A_E2E_CHAIN_ID --mnemonic-file /root/.hermes/KIICHAIN_A_E2E_RLY_MNEMONIC.txt
+hermes keys add --hd-path "m/44'/60'/0'/0/0" --key-name rly01-kiichain-a  --chain $KIICHAIN_A_E2E_CHAIN_ID --mnemonic-file /root/.hermes/KIICHAIN_A_E2E_RLY_MNEMONIC.txt
+
+# Run a health check
+hermes health-check
