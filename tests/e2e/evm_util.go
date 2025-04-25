dee2aa44718ebccc57681807e712eb0f18f609c9
Thales Zirbel <thales.zirbel@brickabode.com>
2025-04-25 11:00:16 -0300

    feat: get contract test

---
+++ tests/e2e/evm_util.go
@@ -0,0 +1,176 @@
+package e2e
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"math/big"
+	"strings"
+	"time"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
+	geth "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func sendEVM(
+	client *ethclient.Client,
+	privateKey *ecdsa.PrivateKey,
+	fromAddress common.Address,
+	toAddress common.Address,
+	amount *big.Int,
+) (geth.Receipt, error) {
+	// Get the nonce (transaction count)
+	return EVMTransaction(client, privateKey, fromAddress, toAddress, amount, nil)
+}
+
+func deployContract(
+	client *ethclient.Client,
+	privateKey *ecdsa.PrivateKey,
+	fromAddress common.Address,
+	contractBinary []byte,
+) (common.Address, error) {
+	receipt, err := EVMTransaction(client, privateKey, fromAddress, common.Address{}, big.NewInt(0), contractBinary)
+	if err != nil {
+		return common.Address{}, fmt.Errorf("failed to deploy contract: %w", err)
+	}
+	return receipt.ContractAddress, nil
+}
+
+func EVMTransaction(
+	client *ethclient.Client,
+	privateKey *ecdsa.PrivateKey,
+	fromAddress common.Address,
+	toAddress common.Address,
+	amount *big.Int,
+	contractBinary []byte,
+) (geth.Receipt, error) {
+	// Get the nonce (transaction count)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		return geth.Receipt{}, fmt.Errorf("failed to get nonce: %w", err)
+	}
+
+	// Get suggested gas price
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		return geth.Receipt{}, fmt.Errorf("failed to get gas price: %w", err)
+	}
+
+	// Get chain ID
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		return geth.Receipt{}, fmt.Errorf("failed to get chain ID: %w", err)
+	}
+
+	// Estimate gas
+	estimatedGas, err := client.EstimateGas(context.Background(), ethereum.CallMsg{
+		From: fromAddress,
+		Data: contractBinary,
+	})
+	if err != nil {
+		return geth.Receipt{}, fmt.Errorf("gas estimation failed: %v", err)
+	}
+
+	// Create the transaction
+	tx := geth.NewTransaction(
+		nonce,
+		toAddress,
+		amount, // no ammount
+		estimatedGas,
+		gasPrice,
+		contractBinary, // contract bytes
+	)
+
+	// Sign the transaction
+	signedTx, err := geth.SignTx(tx, geth.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		return geth.Receipt{}, fmt.Errorf("failed to sign transaction: %w", err)
+	}
+
+	// Send the transaction
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		return geth.Receipt{}, fmt.Errorf("failed to send transaction: %w", err)
+	}
+
+	time.Sleep(time.Millisecond * 5000)
+
+	_, receipt, err := checkTransactionByHash(client, signedTx.Hash())
+	if err != nil {
+		return geth.Receipt{}, fmt.Errorf("failed to get receipt: %w", err)
+	}
+	if receipt.Status == 0 {
+		reason, err := getRevertReason(client, signedTx.Hash(), fromAddress)
+		if err != nil {
+			return geth.Receipt{}, fmt.Errorf("failed to get receipt: %w", err)
+		}
+		return geth.Receipt{}, fmt.Errorf("receipt status is 0, reason: %s", reason)
+	}
+	return *receipt, nil
+}
+
+func EVMCallContract(
+	client *ethclient.Client,
+	contractAddress common.Address,
+	data []byte,
+) ([]byte, error) {
+	msg := ethereum.CallMsg{
+		To:   &contractAddress,
+		Data: data,
+	}
+	return client.CallContract(context.Background(), msg, nil)
+}
+
+func checkTransactionByHash(client *ethclient.Client, txHash common.Hash) (*geth.Transaction, *geth.Receipt, error) {
+	// Get the transaction details
+	tx, isPending, err := client.TransactionByHash(context.Background(), txHash)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to get transaction: %w", err)
+	}
+
+	// If transaction is still pending
+	if isPending {
+		return tx, nil, fmt.Errorf("transaction is still pending")
+	}
+
+	// Get the transaction receipt
+	receipt, err := client.TransactionReceipt(context.Background(), txHash)
+	if err != nil {
+		return tx, nil, fmt.Errorf("failed to get receipt: %w", err)
+	}
+
+	return tx, receipt, nil
+}
+
+func getRevertReason(client *ethclient.Client, txHash common.Hash, sender common.Address) (string, error) {
+	// Get the transaction
+	tx, isPending, err := client.TransactionByHash(context.Background(), txHash)
+	if err != nil {
+		return "", err
+	}
+	if isPending {
+		return "", fmt.Errorf("transaction is still pending")
+	}
+
+	// Replay the transaction as a call
+	msg := ethereum.CallMsg{
+		From:     sender,
+		To:       tx.To(),
+		Gas:      tx.Gas(),
+		GasPrice: tx.GasPrice(),
+		Value:    tx.Value(),
+		Data:     tx.Data(),
+	}
+
+	_, err = client.CallContract(context.Background(), msg, nil)
+	if err != nil {
+		// Extract revert reason from error message
+		if strings.Contains(err.Error(), "execution reverted: ") {
+			return fmt.Sprintf("Execution reverted, error: %s", err.Error()), nil
+		}
+		return err.Error(), nil
+	}
+	return "unknown error (possibly out of gas)", nil
+}
