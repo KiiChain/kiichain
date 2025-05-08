673f96132f5356721435af1a6279876a630bcba8
Jhelison Uchoa <jhelisong@gmail.com>
2025-05-08 15:16:57 -0300

    test: start up the wasmd testing

---
+++ tests/e2e/e2e_wasmd_test.go
@@ -0,0 +1,10 @@
+package e2e
+
+// testWasmdCounter runs the wasm tests
+// it tests the following:
+// 1. instantiate a contract
+// 2. execute a contract
+// 3. query a contract
+func (s *IntegrationTestSuite) testWasmdCounter() {
+	panic("unimplemented")
+}
