3117f5310a193e1411be0d9fc787e313519a4c0f
Jhelison Uchoa <jhelisong@gmail.com>
2025-05-19 13:05:19 -0300

    feat: add the bech32 wasmbinding implementation

--- wasmbinding/wasm.go
+++ wasmbinding/wasm.go
@@ -6,8 +6,9 @@ import (
 	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
 
 	evmkeeper "github.com/cosmos/evm/x/vm/keeper"
-
 	evmwasmbinding "github.com/kiichain/kiichain/v1/wasmbinding/evm"
+
+	"github.com/kiichain/kiichain/v1/wasmbinding/bech32"
 	tfbinding "github.com/kiichain/kiichain/v1/wasmbinding/tokenfactory"
 	tokenfactorykeeper "github.com/kiichain/kiichain/v1/x/tokenfactory/keeper"
 )
@@ -21,9 +22,10 @@ func RegisterCustomPlugins(
 	// Register custom query plugins
 	tokenFactoryQueryPlugin := tfbinding.NewQueryPlugin(bank, tokenFactory)
 	evmQueryPlugin := evmwasmbinding.NewQueryPlugin(evmKeeper)
+	bech32QueryPlugin := bech32.NewQueryPlugin()
 
 	// Create the central query plugin
-	queryPlugin := NewQueryPlugin(tokenFactoryQueryPlugin, evmQueryPlugin)
+	queryPlugin := NewQueryPlugin(tokenFactoryQueryPlugin, evmQueryPlugin, bech32QueryPlugin)
 
 	// Register custom message handler decorators
 	queryPluginOpt := wasmkeeper.WithQueryPlugins(&wasmkeeper.QueryPlugins{
