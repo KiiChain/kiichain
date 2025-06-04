7d6451b6ac924fe10f7d5e92a398e18b5bb89cc4
Thales Zirbel <thales.zirbel@brickabode.com>
2025-06-04 18:14:21 -0300

    feat: add genesis related files

---
+++ x/rewards/keeper/genesis.go
@@ -0,0 +1,41 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/kiichain/kiichain/v1/x/rewards/types"
+)
+
+// InitGenesis sets rewards information from genesis
+func (k Keeper) InitGenesis(ctx sdk.Context, data types.GenesisState) {
+	if err := k.RewardPool.Set(ctx, data.RewardPool); err != nil {
+		panic(err)
+	}
+
+	if err := k.Params.Set(ctx, data.Params); err != nil {
+		panic(err)
+	}
+
+	if err := k.RewardReleaser.Set(ctx, data.RewardReleaser); err != nil {
+		panic(err)
+	}
+}
+
+// ExportGenesis returns a GenesisState for a given context and keeper.
+func (k Keeper) ExportGenesis(ctx sdk.Context) *types.GenesisState {
+	rewardPool, err := k.RewardPool.Get(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	params, err := k.Params.Get(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	rewardReleaser, err := k.RewardReleaser.Get(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	return types.NewGenesisState(params, rewardPool, rewardReleaser)
+}
---
+++ x/rewards/types/genesis.go
@@ -0,0 +1,32 @@
+package types
+
+func NewGenesisState(
+	params Params, rp RewardPool, releaser RewardReleaser,
+) *GenesisState {
+	return &GenesisState{
+		Params:         params,
+		RewardPool:     rp,
+		RewardReleaser: releaser,
+	}
+}
+
+// DefaultGenesisState returns the default genesis state of rewards.
+func DefaultGenesisState() *GenesisState {
+	return &GenesisState{
+		RewardPool:     InitialRewardPool(),
+		Params:         DefaultParams(),
+		RewardReleaser: InitialRewardReleaser(),
+	}
+}
+
+// ValidateGenesis validates the genesis state of rewards genesis input
+func ValidateGenesis(gs *GenesisState) error {
+	if err := gs.Params.ValidateBasic(); err != nil {
+		return err
+	}
+
+	if err := gs.RewardPool.ValidateGenesis(); err != nil {
+		return err
+	}
+	return gs.RewardReleaser.ValidateGenesis()
+}
