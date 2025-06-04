f0b91092fe5122480974e9e6a706256babf39b6f
Thales Zirbel <thales.zirbel@brickabode.com>
2025-06-04 18:13:54 -0300

    feat: add querier

---
+++ x/rewards/keeper/grpc_query.go
@@ -0,0 +1,45 @@
+package keeper
+
+import (
+	"context"
+
+	"github.com/kiichain/kiichain/v1/x/rewards/types"
+)
+
+var _ types.QueryServer = Querier{}
+
+type Querier struct {
+	Keeper
+}
+
+func NewQuerier(keeper Keeper) Querier {
+	return Querier{Keeper: keeper}
+}
+
+// Params queries params of rewards module
+func (k Querier) Params(ctx context.Context, _ *types.QueryParamsRequest) (*types.QueryParamsResponse, error) {
+	params, err := k.Keeper.Params.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.QueryParamsResponse{Params: params}, nil
+}
+
+// RewardPool queries the reward pool coins
+func (k Querier) RewardPool(ctx context.Context, _ *types.QueryRewardPoolRequest) (*types.QueryRewardPoolResponse, error) {
+	pool, err := k.Keeper.RewardPool.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &types.QueryRewardPoolResponse{RewardPool: pool}, nil
+}
+
+// RewardReleaser queries the releaser information
+func (k Querier) RewardReleaser(ctx context.Context, _ *types.QueryRewardReleaserRequest) (*types.QueryRewardReleaserResponse, error) {
+	releaser, err := k.Keeper.RewardReleaser.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &types.QueryRewardReleaserResponse{RewardReleaser: releaser}, nil
+}
