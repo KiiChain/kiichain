1a1a50d41d0d727f13d3b01970292e5b8f76ae6c
Thales Zirbel <thales.zirbel@brickabode.com>
2025-06-06 17:20:19 -0300

    fix: use correct ok return

--- x/rewards/types/params.go
+++ x/rewards/types/params.go
@@ -19,9 +19,9 @@ func DefaultParams() Params {
 // ValidateBasic performs basic validation on distribution parameters.
 func (p Params) ValidateBasic() error {
 	denom := p.TokenDenom
-	minDeposit, err := math.NewIntFromString(p.GovernanceMinDeposit)
-	if err {
-		return fmt.Errorf("invalid string conversion to int on governance min deposit: %s", p.GovernanceMinDeposit)
+	minDeposit, ok := math.NewIntFromString(p.GovernanceMinDeposit)
+	if !ok {
+		return fmt.Errorf("invalid string conversion to int on governance min deposit: %s;", p.GovernanceMinDeposit)
 	}
 	v := sdk.Coin{Denom: denom, Amount: minDeposit}
 	if v.IsValid() {
