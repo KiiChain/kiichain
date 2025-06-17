c9ca82bfa0e8fd556449d14951e484f764932bbb
Jhelison Uchoa <jhelisong@gmail.com>
2025-06-17 20:40:54 -0300

    test: add integration tests

---
+++ precompiles/oracle/integration_test.go
@@ -0,0 +1,51 @@
+package oracle_test
+
+import (
+	"testing"
+	"time"
+
+	tmtypes "github.com/cometbft/cometbft/proto/tendermint/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	testkeyring "github.com/cosmos/evm/testutil/integration/os/keyring"
+	app "github.com/kiichain/kiichain/v1/app"
+	"github.com/kiichain/kiichain/v1/app/helpers"
+	oracleprecompile "github.com/kiichain/kiichain/v1/precompiles/oracle"
+	"github.com/stretchr/testify/suite"
+)
+
+// OraclePrecompileTestSuite is a test suite for the oracle precompile
+type OraclePrecompileTestSuite struct {
+	suite.Suite
+
+	// App and context
+	App     *app.KiichainApp
+	Ctx     sdk.Context
+	keyring testkeyring.Keyring
+
+	// Precompile
+	Precompile *oracleprecompile.Precompile
+}
+
+// TestOraclePrecompileTestSuite runs all the tests under the oracle pre-compile test suite
+func TestOraclePrecompileTestSuite(t *testing.T) {
+	suite.Run(t, new(OraclePrecompileTestSuite))
+}
+
+// SetupSuite sets up the test suite
+func (s *OraclePrecompileTestSuite) SetupSuite() {
+	// Get the test context
+	t := s.T()
+
+	// Create the app and the context
+	s.App = helpers.Setup(t)
+	s.Ctx = s.App.BaseApp.NewUncachedContext(true, tmtypes.Header{Height: 1, ChainID: "test_1010-1", Time: time.Now().UTC()})
+
+	// Start a new keyring
+	keyring := testkeyring.New(2)
+	s.keyring = keyring
+
+	// Start the precompile
+	pc, err := oracleprecompile.NewPrecompile(s.App.OracleKeeper, s.App.AuthzKeeper)
+	s.Require().NoError(err)
+	s.Precompile = pc
+}
