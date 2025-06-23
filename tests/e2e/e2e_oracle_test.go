8b6dcbf3acd5952970828b8b98843254019398f0
Jhelison Uchoa <jhelisong@gmail.com>
2025-06-23 13:56:57 -0300

    test: start the oracle e2e tests

---
+++ tests/e2e/e2e_oracle_test.go
@@ -0,0 +1,20 @@
+package e2e
+
+// Test the following:
+// - Feeless TXs
+// - Double Feeless TXs sending
+// - Slashing
+// - Voting
+// - Feeder address
+
+func (s *IntegrationTestSuite) testFeelessTx() {
+	s.T().Skip("IMPLEMENT ME")
+}
+
+func (s *IntegrationTestSuite) testFeeder() {
+	s.T().Skip("IMPLEMENT ME")
+}
+
+func (s *IntegrationTestSuite) testSlash() {
+	s.T().Skip("IMPLEMENT ME")
+}
