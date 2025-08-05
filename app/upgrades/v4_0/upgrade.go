466521ee8a8e03c15cd2682082e6b1109a509e8c
Jhelison Uchoa <jhelisong@gmail.com>
2025-08-05 14:52:09 -0300

    feat: add v4.0.0 upgrade handler

---
+++ app/upgrades/v4_0/upgrade.go
@@ -0,0 +1,35 @@
+package v400
+
+import (
+	"context"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	"github.com/kiichain/kiichain/v3/app/keepers"
+)
+
+// CreateUpgradeHandler creates the upgrade handler for the v4.0.0 upgrade
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	keepers *keepers.AppKeepers,
+) upgradetypes.UpgradeHandler {
+	return func(c context.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		// State the context and log
+		ctx := sdk.UnwrapSDKContext(c)
+		ctx.Logger().Info("Starting module migrations...")
+
+		// Run the module migrations, it will start the new module with it's init genesis
+		vm, err := mm.RunMigrations(ctx, configurator, vm)
+		if err != nil {
+			return vm, err
+		}
+
+		// Log the upgrade completion
+		ctx.Logger().Info("Upgrade v4.0.0 complete")
+		return vm, nil
+	}
+}
