9963f45057f41ebed522b3090de4c273b36d763e
Thales Zirbel <thales.zirbel@brickabode.com>
2025-11-13 16:02:17 -0300

    docs: add log on migration

--- x/feeabstraction/keeper/migrations.go
+++ x/feeabstraction/keeper/migrations.go
@@ -14,6 +14,7 @@ func NewMigrator(keeper Keeper) Migrator {
 
 // Migrate1to2 migrates the params, removing fallback_native_price from the params
 func (m Migrator) Migrate1to2(ctx sdk.Context) error {
+	ctx.Logger().Info("Migrating fee abstraction v1 params")
 	// Fetch old params, extra param won't be unmarshaled
 	params, err := m.keeper.Params.Get(ctx)
 	if err != nil {
@@ -21,5 +22,9 @@ func (m Migrator) Migrate1to2(ctx sdk.Context) error {
 	}
 
 	// override
-	return m.keeper.Params.Set(ctx, params)
+	err = m.keeper.Params.Set(ctx, params)
+	if err == nil {
+		ctx.Logger().Info("Params migrated successfully")
+	}
+	return err
 }
