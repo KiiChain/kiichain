7e014be8c54cbdb99e143bcd122145bbadc9de64
Thales Zirbel <thales.zirbel@brickabode.com>
2025-11-13 10:12:42 -0300

    feat: try simple migration

--- x/feeabstraction/keeper/migrations.go
+++ x/feeabstraction/keeper/migrations.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/kiichain/kiichain/v5/x/feeabstraction/types"
 )
 
 type Migrator struct {
@@ -13,10 +14,25 @@ func NewMigrator(keeper Keeper) Migrator {
 }
 
 func (m Migrator) Migrate1to2(ctx sdk.Context) error {
+	// storeServie := runtime.NewKVStoreService(appKeepers.keys[feeabstractiontypes.StoreKey])
+	// sb := collections.NewSchemaBuilder(m.keeper.storeService)
+
+	// // Fetch old params, that have one less field
+	// schema := collections.NewItem(sb, types.ParamsKey, "params", codec.CollValue[v1.Params](m.keeper.cdc))
+
 	oldParams, err := m.keeper.Params.Get(ctx)
 	if err != nil {
 		return err
 	}
 
-	return m.keeper.Params.Set(ctx, oldParams)
+	newParams := types.Params{
+		NativeDenom:        oldParams.NativeDenom,
+		NativeOracleDenom:  oldParams.NativeOracleDenom,
+		Enabled:            oldParams.Enabled,
+		ClampFactor:        oldParams.ClampFactor,
+		TwapLookbackWindow: oldParams.TwapLookbackWindow,
+	}
+
+	// Set them up, the extra field should not matter
+	return m.keeper.Params.Set(ctx, newParams)
 }
