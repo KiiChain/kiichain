529980567f9af3744cdfeafa9856282155272f5c
Thales Zirbel <thales.zirbel@brickabode.com>
2025-12-10 14:31:58 -0300

    feat: add custom broadcast

--- app/evm_mempool.go
+++ app/evm_mempool.go
@@ -1,19 +1,25 @@
 package kiichain
 
 import (
+	"fmt"
+
 	"cosmossdk.io/log"
+
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
 	sdkmempool "github.com/cosmos/cosmos-sdk/types/mempool"
 
 	evmmempool "github.com/cosmos/evm/mempool"
+	evmtypes "github.com/cosmos/evm/x/vm/types"
 )
 
 // SetupEVMMempool creates and sets the EVM mempool
 func (app *KiichainApp) SetupEVMMempool(appOpts servertypes.AppOptions, logger log.Logger) {
 	mempoolConfig := &evmmempool.EVMMempoolConfig{
-		AnteHandler: app.GetAnteHandler(),
+		AnteHandler:   app.GetAnteHandler(),
+		BroadCastTxFn: app.broadcastEVMTransactions,
 	}
 
 	evmMempool := evmmempool.NewExperimentalEVMMempool(app.CreateQueryContext, logger, app.EVMKeeper, app.FeeMarketKeeper, app.txConfig, app.clientCtx, mempoolConfig)
@@ -32,4 +38,34 @@ func (app *KiichainApp) SetupEVMMempool(appOpts servertypes.AppOptions, logger l
 	abciProposalHandler := baseapp.NewDefaultProposalHandler(evmMempool, app)
 	abciProposalHandler.SetSignerExtractionAdapter(evmmempool.NewEthSignerExtractionAdapter(sdkmempool.NewDefaultSignerExtractionAdapter()))
 	app.SetPrepareProposal(abciProposalHandler.PrepareProposalHandler())
+}
+
+// broadcastEVMTransactions converts Ethereum transactions to Cosmos SDK format and broadcasts them.
+// This function wraps EVM transactions in MsgEthereumTx messages and submits them to the network
+// using the provided client context. It handles encoding and error reporting for each transaction.
+// TODO: Remove once EVM pushes a fix
+func (app *KiichainApp) broadcastEVMTransactions(ethTxs []*ethtypes.Transaction) error {
+	for _, ethTx := range ethTxs {
+		msg := &evmtypes.MsgEthereumTx{}
+		msg.FromEthereumTx(ethTx)
+
+		txBuilder := app.txConfig.NewTxBuilder()
+		if err := txBuilder.SetMsgs(msg); err != nil {
+			return fmt.Errorf("failed to set msg in tx builder: %w", err)
+		}
+
+		txBytes, err := app.txConfig.TxEncoder()(txBuilder.GetTx())
+		if err != nil {
+			return fmt.Errorf("failed to encode transaction: %w", err)
+		}
+
+		res, err := app.clientCtx.BroadcastTxSync(txBytes)
+		if err != nil {
+			return fmt.Errorf("failed to broadcast transaction %s: %w", ethTx.Hash().Hex(), err)
+		}
+		if res.Code != 0 {
+			return fmt.Errorf("transaction %s rejected by mempool: code=%d, log=%s", ethTx.Hash().Hex(), res.Code, res.RawLog)
+		}
+	}
+	return nil
 }
