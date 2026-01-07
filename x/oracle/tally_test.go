4e1ada5fcf715f5069281f08bc4abaf8d209992a
Menerapkan1 <117538854+Winnode@users.noreply.github.com>
2026-01-07 22:03:23 +0100

    test: use require.NoError instead of blank identifier

--- x/oracle/tally_test.go
+++ x/oracle/tally_test.go
@@ -97,7 +97,8 @@ func TestPickReferenceDenom(t *testing.T) {
 	}
 
 	// Must return denom atomDenom and kiiBallot as below threshold map
-	referenceDenom, belowThresholdVoteMap, _ := pickReferenceDenom(ctx, oracleKeeper, votingTarget, voteMap)
+	referenceDenom, belowThresholdVoteMap, err := pickReferenceDenom(ctx, oracleKeeper, votingTarget, voteMap)
+	require.NoError(t, err)
 	require.Equal(t, utils.AtomDenom, referenceDenom)
 	require.Equal(t, expectedBelowThreshold, belowThresholdVoteMap)
 }
