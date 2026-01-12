735b7e9aa2f8b4d2014197e00d635b4832e44222
alto <raddenpattah@gmail.com>
2026-01-12 17:39:26 +0700

    tests(e2e): make timeout configurable; increase to 3 minutes

--- tests/e2e/e2e_exec_test.go
+++ tests/e2e/e2e_exec_test.go
@@ -732,7 +732,9 @@ func (s *IntegrationTestSuite) expectErrExecValidation(chain *chain, valIdx int,
 				gotErr := queryKiichainTx(endpoint, txResp.TxHash) != nil
 				return gotErr == expectErr
 			},
-			time.Minute,
+			// TODO: make this timeout configurable in the future.
+			// Increased to 3 minutes to mitigate intermittent "height:0" failures.
+			3*time.Minute,
 			5*time.Second,
 			"stdOut: %s, stdErr: %s",
 			string(stdOut), string(stdErr),
@@ -753,7 +755,9 @@ func (s *IntegrationTestSuite) defaultExecValidation(chain *chain, valIdx int) f
 				func() bool {
 					return queryKiichainTx(endpoint, txResp.TxHash) == nil
 				},
-				time.Minute,
+				// TODO: make this timeout configurable in the future.
+				// Increased to 3 minutes to mitigate intermittent "height:0" failures.
+				3*time.Minute,
 				5*time.Second,
 				"stdOut: %s, stdErr: %s",
 				string(stdOut), string(stdErr),
