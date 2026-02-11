0b15a4f8665d54e5610342bb816ad01b00be893b
Thales Zirbel <thales.zirbel@brickabode.com>
2026-02-11 19:28:55 -0300

    feat: add upgrade v7.0.1

--- app/app.go
+++ app/app.go
@@ -68,7 +68,7 @@ import (
 	kiiante "github.com/kiichain/kiichain/v7/ante"
 	"github.com/kiichain/kiichain/v7/app/keepers"
 	"github.com/kiichain/kiichain/v7/app/upgrades"
-	v7_0 "github.com/kiichain/kiichain/v7/app/upgrades/v7_0"
+	v7_0_1 "github.com/kiichain/kiichain/v7/app/upgrades/v7_0_1"
 	"github.com/kiichain/kiichain/v7/client/docs"
 )
 
@@ -78,7 +78,7 @@ var (
 
 	// Upgrades is a list of all the upgrades that are available for the application.
 	Upgrades = []upgrades.Upgrade{
-		v7_0.Upgrade,
+		v7_0_1.Upgrade,
 	}
 )
 
---
+++ app/upgrades/v7_0_1/constants.go
@@ -0,0 +1,16 @@
+package v701
+
+import (
+	"github.com/kiichain/kiichain/v7/app/upgrades"
+)
+
+const (
+	// UpgradeName is the name of the upgrade
+	UpgradeName = "v7.0.1"
+)
+
+// Upgrade defines the upgrade, nothing is done here
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+}
