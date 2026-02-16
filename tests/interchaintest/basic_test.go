257505bd50e95b6f5184981e60ca848aa428db60
Thales Zirbel <thales.zirbel@brickabode.com>
2026-02-16 19:02:09 -0300

    fix: fund faucet

--- tests/interchaintest/basic_test.go
+++ tests/interchaintest/basic_test.go
@@ -2,13 +2,16 @@ package interchaintest
 
 import (
 	"context"
+	"math/big"
 	"testing"
 	"time"
 
 	sdkmath "cosmossdk.io/math"
 	"github.com/cosmos/interchaintest/v10"
 	"github.com/cosmos/interchaintest/v10/chain/cosmos"
+	"github.com/cosmos/interchaintest/v10/ibc"
 	"github.com/cosmos/interchaintest/v10/testreporter"
+	"github.com/cosmos/interchaintest/v10/testutil"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zaptest"
 )
@@ -43,8 +46,26 @@ func TestBasicChain(t *testing.T) {
 		_ = ic.Close()
 	})
 
+	faucetAddr, err := chain.Validators[0].KeyBech32(ctx, interchaintest.FaucetAccountKeyName, "acc")
+	require.NoError(t, err)
+
+	// Fund the faucet from validator
+	err = chain.Validators[0].BankSend(ctx, "validator", ibc.WalletAmount{
+		Address: faucetAddr,
+		Amount: sdkmath.NewIntFromBigInt(new(big.Int).Mul(
+			big.NewInt(100_000_000_000), // 100B tokens
+			new(big.Int).Exp(big.NewInt(10), big.NewInt(18), nil),
+		)),
+		Denom: Denom,
+	})
+	require.NoError(t, err)
+
+	// Wait for transaction to process
+	err = testutil.WaitForBlocks(ctx, 2, chain)
+	require.NoError(t, err)
+
 	// Use amount that faucet can afford with zero gas fees
-	amt := sdkmath.NewInt(50_000_000_000_000_000) //
+	amt := sdkmath.NewInt(50_000_000_000_000) // 50T akii
 	users := interchaintest.GetAndFundTestUsers(t, ctx, "default", amt,
 		chain,
 	)
